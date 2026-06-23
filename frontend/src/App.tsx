62e5cf4dfa72671dcb1cdbf2d6a0647c06d16528
Vanshika Pal <paln6823@gmail.com>
2026-06-23 17:26:56 +0530

    Sprint 1.2: Setup frontend with React, Tailwind and Router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+   
+       <main className="min-h-screen flex items-center justify-center bg-slate-50">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-blue-600">
+          Welcome to DreamSetu 🚀
+        </h1>
+
+        <p className="mt-4 text-lg text-gray-600">
+          Learn • Build • Collaborate • Grow
+        </p>
+
+        <button className="mt-8 rounded-xl bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700">
+          Start Your Journey
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
